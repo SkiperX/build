1e076712cc66a15dddad23068dbdb130fcd434dc
Александр Распономарев <sanya2128@gmail.com>
2019-03-29 15:24:07 +0500

    Initial commit

---
+++ temp/symbols-data-template.js
@@ -0,0 +1,108 @@
+__iconsData: {
+    
+        'age': {
+            width: '13px',
+            height: '14px'
+        },
+    
+        'children': {
+            width: '12px',
+            height: '14px'
+        },
+    
+        'close': {
+            width: '22px',
+            height: '22px'
+        },
+    
+        'cloudy': {
+            width: '397.401px',
+            height: '397.401px'
+        },
+    
+        'comment': {
+            width: '14px',
+            height: '12px'
+        },
+    
+        'date': {
+            width: '14px',
+            height: '14px'
+        },
+    
+        'dot': {
+            width: '32px',
+            height: '32px'
+        },
+    
+        'eat': {
+            width: '14px',
+            height: '14px'
+        },
+    
+        'email': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'location': {
+            width: '16px',
+            height: '23px'
+        },
+    
+        'meteo': {
+            width: '51px',
+            height: '46px'
+        },
+    
+        'people': {
+            width: '16px',
+            height: '14px'
+        },
+    
+        'person': {
+            width: '13px',
+            height: '14px'
+        },
+    
+        'photo': {
+            width: '33px',
+            height: '27px'
+        },
+    
+        'sport': {
+            width: '14px',
+            height: '14px'
+        },
+    
+        'storm': {
+            width: '504px',
+            height: '504px'
+        },
+    
+        'sunny': {
+            width: '399.079px',
+            height: '399.079px'
+        },
+    
+        'swiper': {
+            width: '34px',
+            height: '34px'
+        },
+    
+        'tel': {
+            width: '14px',
+            height: '14px'
+        },
+    
+        'up': {
+            width: '444px',
+            height: '444px'
+        },
+    
+        'video': {
+            width: '32px',
+            height: '32px'
+        },
+    
+}
